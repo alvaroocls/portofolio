f9ea60c4318584be8edbd4d387a1143be2a5b495
Alvaro <alvarocr1703@gmail.com>
2024-10-02 13:19:36 +0700

    reduce code in app.js

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('show');
-      } else {
-        if (entry.target.classList.contains('hidden2')) {
-          entry.target.classList.remove('show');
-          entry.target.classList.add('hidden2');
-        } else {
-          entry.target.classList.remove('show');
-          entry.target.classList.add('hidden');
-        }
-      }
-    });
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }else{
+      entry.target.classList.remove('show');
+      entry.target.classList.add('hidden');
+    }
   });
+});
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach(element => {
